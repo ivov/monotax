4af3852bf11d6f2d4ae1e1a2e338bc93fbeb0059
Iván Ovejero <ivan_ovejero@hotmail.com>
2020-02-25 23:21:42 -0300

    Refactor to simplify main routes

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,11 @@
 import express from "express";
 import cors from "cors";
-import invoiceRouter from "./routes/invoicesRoute";
-import earningsRouter from "./routes/earningsRoute";
-import expensesRouter from "./routes/expensesRoute";
-import savingsRouter from "./routes/savingsRoute";
-import adwordsRouter from "./routes/adwordsRoute";
+import setRoutes from "./routes/mainRouteCreator";
 
 const app = express();
 
 app.use(cors());
-
-app.use("/api/invoices", invoiceRouter);
-app.use("/api/earnings", earningsRouter);
-app.use("/api/expenses", expensesRouter);
-app.use("/api/savings", savingsRouter);
-app.use("/api/adwords", adwordsRouter);
+setRoutes(app);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`App listening on port ${port}`));
--- src/utils.ts
+++ src/utils.ts
@@ -3,15 +3,6 @@ export const padWithZeros = (number: string) => {
   return pad.substring(0, pad.length - number.length) + number;
 };
 
-/* Collection of react-admin views and their corresponding views in monotax.db*/
-export const VIEWS: { [key: string]: string } = {
-  INVOICES: "invoices_with_tax_id",
-  EARNINGS: "earned_amounts_in_ars_and_usd",
-  SAVINGS: "savings_per_month_and_year",
-  EXPENSES: "expense_amounts_in_ars_and_usd",
-  ADWORDS: "adwords_amounts_in_ars_and_usd"
-};
-
 export const extractYearMonthAndDay = (invoiceDate: Date) => {
   const rawMonth = (invoiceDate.getMonth() + 1).toString();
   const rawDay = invoiceDate.getDate().toString();
